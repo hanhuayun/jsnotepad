5df190dee02c9595eb0e71bbeccfaf4dbb850d85
hanhuayun <2256988633@qq.com>
2021-04-18 09:29:51 +0800

    ok

--- com/editor/editor.js
+++ com/editor/editor.js
@@ -1,5 +1,3 @@
-/* global np: true */
-/* exported $editor */
 let $editor = (() => {
   let $DOM = $(`
     <div class="notepad-editor">
@@ -77,6 +75,43 @@ let $editor = (() => {
     np.setFontStyle($textArea, e.style);
   };
 
+  let selectAll = () => {
+    let n = $textArea.val().length;
+
+    $textArea[0].selectionStart = 0;
+    $textArea[0].selectionEnd = n;
+
+    $textArea.select();
+  };
+
+  let insertDataTime = () => {
+    let str = $textArea.val();
+
+    let strLeft = str.substring(0, $textArea[0].selectionStart),
+        strRight = str.substring($textArea[0].selectionEnd, str.length);
+
+    str = strLeft + new Date().toLocaleString() + strRight;
+
+    $textArea.val(str);
+    $textArea.focus();
+    cfg.posHandler(getRow(), getCol());
+  };
+
+  let gotoLn = (num) => {
+    let str = $textArea.val(),
+        m   = 0;
+
+    let aryStr = str.split('\n');
+    for(let i=0; i<num-1; i++) {
+      m += aryStr[i].length + 1;
+    }
+
+    $textArea[0].selectionStart = m;
+    $textArea[0].selectionEnd = m;
+    $textArea.focus();
+    cfg.posHandler(getRow(), getCol());
+  };
+
   let bingSearch = () => {
     let start = $textArea[0].selectionStart,
         end   = $textArea[0].selectionEnd;
@@ -87,6 +122,36 @@ let $editor = (() => {
       let subStr = $textArea.val().substring(start, end);
       window.open('https://cn.bing.com/search?q=' + subStr, '_blank');
     }
+  };
+
+  let search = (srch) => {
+    let content  = $textArea.val(),
+        srchCtnt = srch.content;
+
+    if(!srch.capitalSense) { // 不区分大小写，把所有字符串都转换成小写
+      content  = content.toLowerCase();
+      srchCtnt = srchCtnt.toLowerCase();
+    }
+
+    let start = $textArea[0].selectionEnd;
+    let result;
+
+    if(srch.direction === 'down') { // 查找方向，向下
+      result = content.indexOf(srchCtnt, start);
+    } else { // srch.direction === 'up'，查找方向，向上
+      let subStr = content.substr(0, $textArea[0].selectionStart);
+      result = subStr.lastIndexOf(srchCtnt);
+    }
+
+    if(result === -1) {
+      alert('找不到 "' + srch.content + '"');
+      return;
+    }
+
+    $textArea[0].selectionStart = result;
+    $textArea[0].selectionEnd = result + srchCtnt.length;
+
+    cfg.posHandler(getRow(), getCol());
   };
 
   let newFile    = () => $textArea.val('');
@@ -112,7 +177,11 @@ let $editor = (() => {
     setContent,
     setWrap,
     newFile,
+    selectAll,
+    insertDataTime,
+    gotoLn,
     bingSearch,
+    search,
     setFont
   };
 })();
