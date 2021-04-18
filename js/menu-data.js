b8aa9f66fb7a85c093286e89e82cc3320a4e2f81
hanhuayun <2256988633@qq.com>
2021-04-18 10:51:40 +0800

    ok

--- js/menu-data.js
+++ js/menu-data.js
@@ -134,7 +134,26 @@ np.menuData = [{
     title: '自动换行(W)',
     shortcut: '',
     enabled: true,
-    handler: () =>console.log('自动换行 clicked!')
+    handler: () => {
+      np.bWrap = !np.bWrap;
+      localStorage.setItem('bWrap', np.bWrap);
+
+      if(np.bWrap) {
+        $statusBar.display(false);
+        $editor.resize(false);
+        $menubar.enabled(3, 0, false);   // [查看]-[状态栏]菜单禁用
+        $menubar.enabled(1, 9, false);   // [编辑]-[转到]菜单禁用
+      } else {
+        $statusBar.display(np.bShowStatusBar);
+        $editor.resize(np.bShowStatusBar);
+        $menubar.enabled(3, 0, true);     // [查看]-[状态栏]菜单启用
+        $menubar.enabled(1, 9, true);     // [编辑]-[转到]菜单启用
+        $menubar.checked(3, 0, np.bShowStatusBar);
+      }
+
+      $menubar.checked(2, 0, np.bWrap);
+      $editor.setWrap(np.bWrap);
+    }
   }, {
     title: '字体(F)...',
     shortcut: '',
